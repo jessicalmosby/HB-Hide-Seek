d1c869502b09905fc0d61503000aeacad3aca59e
Jessica Mosby <jessicamosby@mac.com>
2022-10-31 18:00:25 -0400

    incremented guesses

--- app.js
+++ app.js
@@ -36,22 +36,30 @@ boulderButton.addEventListener('click', () => {
 });
 
 function handleGuess(correctSpot, userGuess) {
-    // reset the styles
-    // then increment the guesses
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
-}
+    resetStyles();
+    totalGuesses++;
 
-function reset() {
-    tree.classList.remove('reveal');
-    boulder.classList.remove('reveal');
-    shed.classList.remove('reveal');
-}
+    const correctHidingPlaceEL = document.getElementById('${correctSpot}-container');
 
-function displayResults() {
-    winsEl.textContent = wins;
-    lossesEl.textContent = total - wins;
-    totalEl.textContent = total;
+    correctHidingPlaceEL.classList.add('face');
+
+    if (correctSpot === userGuess) {
+        correctGuesses++;
+    }
+
+    function resetStyles() {
+        treeContainer.classList.remove('face');
+        boulderContainer.classList.remove('face');
+        shedContainer.classList.remove('face');
+    }
+
+    function displayResults() {
+        winsEl.textContent = wins;
+        lossesEl.textContent = total - wins;
+        totalEl.textContent = total;
+    }
 }
