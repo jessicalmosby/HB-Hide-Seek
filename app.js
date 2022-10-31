4c031cd86b28a5384d3b28b85c43f12dab22396c
Jessica Mosby <jessicamosby@mac.com>
2022-10-31 15:42:53 -0400

    added results function and reset function

--- app.js
+++ app.js
@@ -43,3 +43,15 @@ function handleGuess(correctSpot, userGuess) {
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
 }
+
+function reset() {
+    tree.classList.remove('reveal');
+    boulder.classList.remove('reveal');
+    shed.classList.remove('reveal');
+}
+
+function displayResults() {
+    winsEl.textContent = wins;
+    lossesEl.textContent = total - wins;
+    totalEl.textContent = total;
+}
